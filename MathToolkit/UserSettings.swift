3d05028fb747007f2e9d39209e13d5a059bddf75
Sweeper777 <sumulang@gmail.com>
2018-03-15 07:28:19 +0000

    added valueOfPi

--- MathToolkit/UserSettings.swift
+++ MathToolkit/UserSettings.swift
@@ -1,4 +1,18 @@
 import Foundation
 
 class UserSettings {
+    static var valueOfPi: Double {
+        get {
+            if UserDefaults.standard.double(forKey: "valueOfPi") == 0 {
+                UserDefaults.standard.set(Double.pi, forKey: "valueOfPi")
+                return Double.pi
+            } else {
+                return UserDefaults.standard.double(forKey: "valueOfPi")
+            }
+        }
+        
+        set {
+            UserDefaults.standard.set(newValue, forKey: "valueOfPi")
+        }
+    }
 }
