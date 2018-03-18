a098f79d6e54ac63f3bf714b6f67a05233726d78
Sweeper777 <sumulang@gmail.com>
2018-03-18 09:34:06 +0000

    added Threading

---
+++ MathToolkit/Threading.swift
@@ -0,0 +1,23 @@
+import Foundation
+infix operator ~>
+
+/**
+ Executes the lefthand closure on a background thread and,
+ upon completion, the righthand closure on the main thread.
+ Passes the background closure's output, if any, to the main closure.
+ */
+func ~> <R> (
+    backgroundClosure: @escaping () -> R,
+    mainClosure:       @escaping (R) -> ())
+{
+    queue.async {
+        let result = backgroundClosure()
+        DispatchQueue.main.async {
+            mainClosure(result)
+        }
+    }
+}
+
+/** Serial dispatch queue used by the ~> operator. */
+private let queue = DispatchQueue(label: "serial-worker")
+
