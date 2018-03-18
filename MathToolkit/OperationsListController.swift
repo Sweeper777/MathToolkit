471449a8bbfe655456a7bc2585e48378ee7e2f16
Sweeper777 <sumulang@gmail.com>
2018-03-18 09:35:31 +0000

    added loadOperations

--- MathToolkit/OperationsListController.swift
+++ MathToolkit/OperationsListController.swift
@@ -5,5 +5,15 @@ class OperationsListController: UITableViewController {
 
     var operations: [[OperationProtocol]] = []
     
+    func loadOperations(completion: @escaping () -> Void) {
+        {
+            [weak self] in
+            let jsonData = try! Data(contentsOf: Bundle.main.url(forResource: "operations", withExtension: "json")!)
+            let decoder = JSONDecoder()
+            let operationsList = try! decoder.decode([JsonOperation].self, from: jsonData)
+            self?.operations = operationsList.groupBy { $0.category }
+        } ~> completion
+    }
+    
 }
 
