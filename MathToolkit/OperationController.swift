8b1ec16af0d7c1c72134ceccd7b4a65ead7cd193
Sweeper777 <sumulang@gmail.com>
2018-03-20 20:48:14 +0000

    added rows for inputs

--- MathToolkit/OperationController.swift
+++ MathToolkit/OperationController.swift
@@ -6,4 +6,24 @@ import RFKeyboardToolbar
 class OperationController: FormViewController {
     var operation: OperationProtocol!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let op = operation else {
+            return
+        }
+        self.title = op.name.localized
+        
+        let section = Section("inputs".localized)
+        
+        for input in operation.inputs {
+            section <<< TextRow(input.name) {
+                row in
+                row.cell.textField.placeholder = "Enter a value...".localized
+                row.title = "\(input.name) = "
+                row.cell.titleLabel?.font = UIFont(name: "Times New Roman", size: UIFont.systemFontSize)
+            }
+        }
+        
+        form +++ section
+    }
 }
