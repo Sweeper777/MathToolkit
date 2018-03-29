80472fe12b1cd1ddf9d9687b0efeac5a1d24b801
Sweeper777 <sumulang@gmail.com>
2018-03-29 07:13:48 +0100

    added sigFigRow

--- MathToolkit/SettingsController.swift
+++ MathToolkit/SettingsController.swift
@@ -3,8 +3,21 @@ import Eureka
 
 class SettingsController: FormViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        form +++ Section("significant figures option".localized)
+        form.allSections.last! <<< PickerRow<String>(tagSigiFigOption) {
+            row in
+            row.title = "Significant Figures"
+            row.options = ["As accurate as possible", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+            row.value = "As accurate as possible"
+        }
+    }
+    
     @IBAction func done() {
         self.dismiss(animated: true, completion: nil)
     }
 }
 
+let tagSigiFigOption = "sigFigOption"
