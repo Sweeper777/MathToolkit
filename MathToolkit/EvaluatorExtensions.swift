a8104ec9b43fa82ed2a0840a4d47fe56410f6a63
Sweeper777 <sumulang@gmail.com>
2018-03-29 07:12:21 +0100

    added approxEq function

--- MathToolkit/EvaluatorExtensions.swift
+++ MathToolkit/EvaluatorExtensions.swift
@@ -1,9 +1,19 @@
 import MathParser
+import Foundation
 
 extension Evaluator {
     static var shared: Evaluator = {
         var evaluator = Evaluator()
-        // TODO: register functions
+        
         return evaluator
     }()
+}
+
+func approxEqImpl(state: EvaluationState) throws -> Double {
+    if state.arguments.count != 2 {
+        throw MathParserError(kind: .invalidArguments, range: Range(0...0))
+    }
+    let lhs = try state.evaluator.evaluate(state.arguments.first!, substitutions: state.substitutions)
+    let rhs = try state.evaluator.evaluate(state.arguments[1], substitutions: state.substitutions)
+    return (abs(lhs - rhs) < 0.00000000001) ? 1 : 0
 }
