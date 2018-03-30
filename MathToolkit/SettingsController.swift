394d7ca721963a7fa1edf8777ba6dedf059ed4b3
Sweeper777 <sumulang@gmail.com>
2018-03-30 09:25:38 +0100

    added onChange for sigFigOptions row

--- MathToolkit/SettingsController.swift
+++ MathToolkit/SettingsController.swift
@@ -9,10 +9,18 @@ class SettingsController: FormViewController {
         form +++ Section("significant figures option".localized)
         form.allSections.last! <<< PickerInlineRow<String>(tagSigiFigOption) {
             row in
-            row.title = "Significant Figures"
-            row.options = ["As accurate as possible", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-            row.value = "As accurate as possible"
-        }
+            row.title = "Significant Figures".localized
+            row.options = ["As accurate as possible", "1", "2", "3", "4", "5", "6", "7", "8", "9"].map { $0.localized }
+            row.value = "As accurate as possible".localized
+        }.onChange({ (row) in
+            if let index = row.options.index(of: row.value!) {
+                if index == 0 {
+                    UserSettings.sigFigOption = .no
+                } else if index > 0 {
+                    UserSettings.sigFigOption = .yes(index)
+                }
+            }
+        })
     }
     
     @IBAction func done() {
