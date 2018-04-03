e63284ef2cba852b1ffadd46fb365743456c8fb3
Sweeper777 <sumulang@gmail.com>
2018-04-03 08:32:16 +0100

    removed do...catch statement

--- MathToolkit/Operation.swift
+++ MathToolkit/Operation.swift
@@ -49,36 +49,30 @@ struct JsonOperation: Codable, OperationProtocol {
     func calculate(inputs: [String : Double]) -> [OperationResult]? {
         var evaluator = Evaluator.shared
         evaluator.angleMeasurementMode = UserSettings.useDegrees ? .degrees : .radians
-        let approxEq = Function(name: "approxEq", evaluator: approxEqImpl)
-        try! evaluator.registerFunction(approxEq)
         var results = [OperationResult]()
         var substitutions = inputs
         substitutions["pref90"] = UserSettings.pref90Degrees
         substitutions["pref180"] = UserSettings.pref180Degrees
         substitutions["pi"] = UserSettings.valueOfPi
         for impl in implementations {
-            if let expression = try? Expression(string: impl.expression) {
-                do {
-                    let result = try evaluator.evaluate(expression, substitutions: substitutions)
-                    let fromValues = String(format: "From %@".localized, impl.fromValues.joined(separator: ", "))
-                    if impl.resultName.hasPrefix("'") && impl.resultName.hasSuffix("'") {
-                        let value = (result == 1 ? "True" : "False").localized
-                        results.append(OperationResult(
-                            name: String(impl.resultName.dropFirst().dropLast()),
-                            from: fromValues,
-                            value: value
-                            )
+            if let expression = try? Expression(string: impl.expression), let result = try? evaluator.evaluate(expression, substitutions: substitutions) {
+                
+                let fromValues = String(format: "From %@".localized, impl.fromValues.joined(separator: ", "))
+                if impl.resultName.hasPrefix("'") && impl.resultName.hasSuffix("'") {
+                    let value = (result == 1 ? "True" : "False")
+                    results.append(OperationResult(
+                        name: String(impl.resultName.dropFirst().dropLast()),
+                        from: fromValues,
+                        value: value
                         )
-                    } else {
-                        results.append(OperationResult(
-                            name: impl.resultName,
-                            from: fromValues,
-                            value: "\(UserSettings.sigFigOption.correct(result))"
-                            )
+                    )
+                } else {
+                    results.append(OperationResult(
+                        name: impl.resultName,
+                        from: fromValues,
+                        value: "\(UserSettings.sigFigOption.correct(result))"
                         )
-                    }
-                } catch {
-                    print(error)
+                    )
                 }
             }
         }
