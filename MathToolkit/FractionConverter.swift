e87b3688d561b7ef1f2491915140715c97f7381d
Sweeper777 <sumulang@gmail.com>
2018-04-07 09:48:16 +0100

    translated conversion code to swift

--- MathToolkit/FractionConverter.swift
+++ MathToolkit/FractionConverter.swift
@@ -29,3 +29,28 @@ struct Fraction: CustomStringConvertible {
         return "\(numerator) / \(denominator)"
     }
 }
+
+extension Double {
+    func toFraction() -> Fraction {
+        let eps = 0.000_001
+        var numeratorFound = Int(rounded())
+        var denominatorFound = 1
+        var errorFound = abs(self - Double(numeratorFound))
+        for denominator in 2..<100 {
+            if errorFound <= eps {
+                break
+            }
+            var numerator = Int(self * Double(denominator))
+            for _ in 0..<2 {
+                let error = abs(self - (Double(numerator) / Double(denominator)))
+                if error < errorFound {
+                    numeratorFound = numerator
+                    denominatorFound = denominator
+                    errorFound = error
+                }
+                numerator += 1
+            }
+        }
+        return Fraction(numerator: numeratorFound, denominator: denominatorFound)
+    }
+}
