daf63d4ffaf3c5c4d9f60271cb179e4393f2e019
Sweeper777 <sumulang@gmail.com>
2018-05-29 08:19:52 +0100

    tested operation result

--- MathToolkitTests/MathToolkitTests.swift
+++ MathToolkitTests/MathToolkitTests.swift
@@ -3,4 +3,11 @@ import XCTest
 
 class MathToolkitTests: XCTestCase {
     
+    func testOperationResult() {
+        let operationResult = OperationResult(name: "My Result", from: "a, b", value: "100")
+        XCTAssertEqual(operationResult.name, "My Result")
+        XCTAssertEqual(operationResult.from, "a, b")
+        XCTAssertEqual(operationResult.value, "100")
+    }
+    
 }
